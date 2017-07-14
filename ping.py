e59f0775174a7c949a5ecd26e181fb90de4f04c0
Sehrish Jahan <jahan.seh@gmail.com>
2017-07-14 17:07:43 -0400

    modifying ping

--- ping.py
+++ ping.py
@@ -1,11 +1,5 @@
-# import os
-# host = "192.168.2.110"
-# response = os.system('ping -c 1' +host)
+import os
+host = "192.168.2.110"
+response = os.system('ping -c 1' +host)
 
-
-def hello():
-    print("Hello")
-
-x = hello()
-print(x)
 
