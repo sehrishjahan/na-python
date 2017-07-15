c4c25cab7ab0274daefd4df13afc63715278bbb6
sehrishjahan <jahan.seh@gmail.com>
2017-07-15 01:26:56 -0400

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,22 +1,11 @@
 import paramiko
 ssh = paramiko.SSHClient()
 
-session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-session.connect('192.168.2.110', port=22, username='csrv', password='cisco1234')
-#Start an interactive shell session on the router
-        connection = session.invoke_shell()	
-        
-        #Setting terminal length for entire output - disable pagination
-        connection.send("terminal length 0\n")
-        time.sleep(1)
-        
-        #Entering global config mode
-        connection.send("\n")
-        connection.send("configure terminal\n")
-        time.sleep(1)
+ip = '192.168.2.110
+username = 'csrv'
+password = 'cisco1234
 
-#stdin, stdout, stderr = ssh.exec_command('show ip int brief')
-#output = stdout.readlines()
-#type(output)
-#print '\n'.join(output)
+remote_conn_pre=paramiko.SSHClient()
+remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
 
