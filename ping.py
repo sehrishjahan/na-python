990ac5dac23b63d4f5fb6ab16b9b3fb4ca68669c
sehrishjahan <jahan.seh@gmail.com>
2017-07-14 23:25:39 -0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,7 +2,6 @@ import os
 
 def ipcheck():
     status = os.system("ping -c 3 " + s)
-            print "\n# # # # # # # # # # # # # # # # # # # # # # # # # # # #\n"
 
     if status == 0:
         print("System " + s + " is UP !")
