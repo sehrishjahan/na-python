27082a3c0a8b18b4983d0ec10f75a9c078acdc36
sehrishjahan <jahan.seh@gmail.com>
2017-07-14 23:59:57 -0400

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,8 +1,6 @@
 import paramiko
 ssh = paramiko.SSHClient()
 
-ssh.connect('192.168.2.110', port=22, username='csrv', password='cisco1234')
-
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('192.168.2.110', port=22, username='csrv', password='cisco1234')
 stdin, stdout, stderr = ssh.exec_command('show ip int brief')
