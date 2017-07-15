150881f036910a46ce4434149b9893f33d6a95ef
sehrishjahan <jahan.seh@gmail.com>
2017-07-15 00:44:07 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -1,11 +1,11 @@
 from netmiko import ConnectHandler
- from pyIOSXR import IOSXR
 
 platform = 'cisco_ios'
 host = '192.168.2.110'
 username = 'csrv'
 password = 'cisco1234'
-#device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
 
-device = IOSXR(hostname='host', username='csrv', password='cisco1234', port=22, lock=False)
-device.open()
+#net_connect = ConnectHandler(**cisco_881)
+net_connect = ConnectHandler(device_type=platform, ip=host, username='csrv', password='cisco1234')
+net_connect.find_prompt()
+
