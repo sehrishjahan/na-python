a7b756b0995c85d1f8b4724ac03e785b615a00ea
sehrishjahan <jahan.seh@gmail.com>
2017-07-15 02:13:48 -0400

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -6,8 +6,6 @@ password = 'cisco1234'
 
 remote_conn_pre=paramiko.SSHClient()
 remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-remote_conn_pre.connect(ip, port=22, username=username, password=password, look_for_keys=False, allow_agent=False)
-remote_conn = remote_conn_pre.invoke_shell()
-output = remote_conn.recv(1000)
-print output
+remote_conn_pre.connect(ip, port=2222, username=username, password=password, look_for_keys=False, allow_agent=False)
+
 
