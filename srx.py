c5ba39c6d705d0e0c2c4d23c69592126323a201d
kaur-raj <rajvvinder@gmail.com>
2017-07-16 13:05:30 -0400

    Update srx.py

--- srx.py
+++ srx.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 from jnpr.junos import Device
-dev = Device(host='192.168.3.99', user='andrew', password='Andrew')
+dev = Device(host='192.168.2.101', user='JuniperSRX', password='cisco1234')
 dev.open()
 pprint(dev.facts)
 dev.close()
