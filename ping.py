913f49a9083278c5542433042129de789db95451
sehrishjahan <jahan.seh@gmail.com>
2017-07-16 12:31:40 -0400

    Update ping.py

--- ping.py
+++ ping.py
@@ -2,13 +2,10 @@ import os
 
 def ping():
     response = os.system("ping -c 3 " + s)
-
     if response == 0:
         print("System " + s + " is UP !")
     else:
         print("System " + s + " is DOWN !")
-
-
-s = raw_input('Enter the ip address: ')         #Prompting user for ip address
-
-ping()
+        #Prompting user for ip address
+s = raw_input('Enter the ip address: ')  
+ping() #calling ping function
