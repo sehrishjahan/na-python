d2fbe513ec80ba649c27dfda02ded492fd350726
sehrishjahan <jahan.seh@gmail.com>
2017-07-18 10:10:02 -0400

    Update srxssh.py

--- srxssh.py
+++ srxssh.py
@@ -1,4 +1,15 @@
- from netmiko import ConnectHandler
- net_connect=ConnectHandler(device_type='juniper',ip='192.168.2.101',username='juniper',password='juniper123')
- output=net_connect.send_command("show interface terse")
- print output
+from netmiko import ConnectHandler #imported netmiko library
+
+#initialized variables.
+platform = 'juniper'
+host = '192.168.2.101'
+username = 'juniper'
+password = 'juniper123'
+
+#calling ConnectHandler function and passing in variables
+net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', password='juniper123')
+net_connect.find_prompt()
+
+#using send_command() method to send the 'show ip int brief' command to router
+output = net_connect.send_command("show interface terse")
+print output
