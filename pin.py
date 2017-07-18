d20501ddeea3bea22de640f3528922b75dbb8d24
sehrishjahan <jahan.seh@gmail.com>
2017-07-18 11:30:58 -0400

    Update pin.py

--- pin.py
+++ pin.py
@@ -1,5 +1,24 @@
+import sys
 import os
-host = "192.168.2.110"
-response = os.system("ping -c 4 " + host)
+import platform
+import subprocess
 
+plat = platform.system()
+scriptDir = sys.path[0]
+devices = os.path.join(scriptDir, 'devices.txt')
+hostsFile = open(devices, "r")
+lines = hostsFile.readlines()
 
+if plat == "Linux":
+    for line in lines:
+        line = line.strip( )
+        ping = subprocess.Popen(
+            ["ping", "-c", "1", "-l", "1", "-s", "1", "-W", "1", line],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE
+        )
+        out, error = ping.communicate()
+        print out
+        print error
+
+hostsFile.close()
