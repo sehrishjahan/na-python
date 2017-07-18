dd6f5f985359f34f4be8d2b106e012f6b767220d
sehrishjahan <jahan.seh@gmail.com>
2017-07-18 10:42:45 -0400

    Create devices.py

---
+++ devices.py
@@ -0,0 +1,23 @@
+csrv1= {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.11',
+    'username': 'csrv1',
+    'password': 'cisco1234',
+    'port': 22,
+}
+
+csrv2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.12',
+    'username': 'csrv2',
+    'password': 'cisco2',
+    'port': 9722,
+}
+
+csrv3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.13',
+    'username': 'csrv3',
+    'password': 'cisco3',
+    'port': 8622,
+}
