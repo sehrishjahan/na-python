ed065630601c0bfe2d69c6bc13ddfac827479cc5
sehrishjahan <jahan.seh@gmail.com>
2017-07-18 11:00:24 -0400

    Create pingall.py

---
+++ pingall.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import platform
+import subprocess
+
+plat = platform.system()
+scriptDir = sys.path[0]
+devices = os.path.join(scriptDir, 'devices.py')
+hostsFile = open(devices, "r")
+lines = hostsFile.readlines()
+
+if plat == "Linux":
+    for line in lines:
+        line = line.strip( )
+        ping = subprocess.Popen(
+            ["ping", "-c", "1", "-l", "1", "-s", "1", "-W", "1", line],
+            stdout = subprocess.PIPE,
+            stderr = subprocess.PIPE
+        )
+        out, error = ping.communicate()
+        print out
+        print error
+
+hostsFile.close()
