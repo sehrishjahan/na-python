e76436171952cb1bb74d7491eea4ac4a40119f7d
sehrishjahan <jahan.seh@gmail.com>
2017-07-18 11:22:47 -0400

    Update pingall.py

--- pingall.py
+++ pingall.py
@@ -3,20 +3,10 @@ import os
 import platform
 import subprocess
 
-plat = platform.system()
-scriptDir = sys.path[0]
-devices = os.path.join(scriptDir, 'devices.py')
-hostsFile = open(devices, "r")
-lines = hostsFile.readlines()
-
-if plat == "Linux":
-    for line in lines:
-        line = line.strip( )
-        ping = os.system("ping -c 1 ", line)
-
-       
-        out, error = ping.communicate()
-        print out
-        print error
-
-hostsFile.close()
+with open('devices.txt', 'r') as f:
+        for ip in f:
+            result=subprocess.Popen(["ping", "-c", "1", "-n", "-W", "2",    ip],stdout=f, stderr=f).wait()
+            if result:
+                print(ip, "inactive")
+            else:
+                print(ip, "active")
