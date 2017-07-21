cbc2a2bd1186b037cd6974cc569bf8178ba6ac61
kaur-raj <rajvvinder@gmail.com>
2017-07-21 13:47:46 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -12,6 +12,5 @@ net_connect.find_prompt()
 
 #using send_command() method to send the 'show ip int brief' command to router
 output = net_connect.send_command("show ip int brief")
-output = net_connect.send_command("show run")
 print output
 
