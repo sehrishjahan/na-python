ce7c582c53e4cdde7d00dbf0a56fec91d403aaf3
kaur-raj <rajvvinder@gmail.com>
2017-07-21 14:33:02 -0400

    Update connectall.py

--- connectall.py
+++ connectall.py
@@ -1,11 +1,11 @@
 from netmiko import ConnectHandler #imported netmiko library
 from devices import csrv, csrv2, csrv3
- def main ():
+def main ():
   device_list = [csrv, csrv2, csrv3]
   print 
   for a_device in device_list:
-   net_connect = ConnectHandler(**a_device)
-   print "{}: {}.format(net_connect.device_type, net_connect.find_prompt())
+    net_connect = ConnectHandler(**a_device)
+    print "{}: {}.format(net_connect.device_type, net_connect.find_prompt())
  
  if __name__ == "__main__":
  main()
