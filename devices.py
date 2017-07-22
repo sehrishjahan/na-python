fe7fd7335e751b404ebe8a54b65e5104afe30b0c
sehrishjahan <jahan.seh@gmail.com>
2017-07-22 00:59:47 -0400

    Update devices.py

--- devices.py
+++ devices.py
@@ -4,20 +4,20 @@ SSH    = {
     'username': 'csrv',
     'password': 'telnet',
     'port': 22,
-}
+},
 
- 'csrv2': { 
+ 'csrv2':{ 
     'device_type': 'cisco_ios',
     'ip': '192.168.2.12',
     'username': 'csrv2',
     'password': 'telnet',
     'port': 9722,
-}'
+},
 
 'csrv3': {     'device_type': 'cisco_ios',
     'ip': '192.168.2.13',
     'username': 'csrv3',
     'password': 'telnet',
     'port': 8622,
-}
+},
 }
