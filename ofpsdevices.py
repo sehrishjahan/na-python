801e7ee271cb32fed6c5b1a02e4e7d83d4b41bcd
kaur-raj <rajvvinder@gmail.com>
2017-07-22 07:20:42 -0400

    Update ofpsdevices.py

--- ofpsdevices.py
+++ ofpsdevices.py
@@ -10,15 +10,4 @@ csrv3 = {
 'username': 'cisco',
 'password':'cisco1234',
 }
-srx1 = {
-'device_type':'juniper',
-'ip': '192.168.2.14',
-'username': 'juniper',
-'password':'cisco1234',
-}
-srx2 = {
-'device_type':'juniper',
-'ip': '192.168.2.15',
-'username': 'juniper',
-'password':'cisco1234',
-}
+
