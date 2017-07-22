6e0b592ccef85b0a6ef6af569c89832df294cb31
kaur-raj <rajvvinder@gmail.com>
2017-07-22 07:18:57 -0400

    Create ofpsdevices.py

---
+++ ofpsdevices.py
@@ -0,0 +1,24 @@
+csrv2 = {
+'device_type':'cisco_ios',
+'ip': '192.168.2.12',
+'username': 'cisco',
+'password':'cisco1234',
+}
+csrv3 = {
+'device_type':'cisco_ios',
+'ip': '192.168.2.13',
+'username': 'cisco',
+'password':'cisco1234',
+}
+srx1 = {
+'device_type':'juniper',
+'ip': '192.168.2.14',
+'username': 'juniper',
+'password':'cisco1234',
+}
+srx2 = {
+'device_type':'juniper',
+'ip': '192.168.2.15',
+'username': 'juniper',
+'password':'cisco1234',
+}
