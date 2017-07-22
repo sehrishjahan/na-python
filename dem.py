f7ecb61e3054039a6405ff6888dd08d834f42b66
kaur-raj <rajvvinder@gmail.com>
2017-07-22 07:43:53 -0400

    Update dem.py

--- dem.py
+++ dem.py
@@ -1,14 +1,7 @@
+from netmiko import ConnectHandler
+from ospfdevices import csrv2
 
 
 
 
 
-def configure_bgp(net_connect, file_name=''):
-    """Configure BGP on device."""
-    try:
-        output = net_connect.send_config_from_file(config_file=file_name)
-        if net_connect.device_type == 'cisco_xr_ssh':
-            output += net_connect.commit()
-        return output
-    except IOError:
-print "Error reading file: {}".format(file_name)
