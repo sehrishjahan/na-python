557f9312130e438a9e1e0948a90393a15c6f3fec
kaur-raj <rajvvinder@gmail.com>
2017-07-22 06:25:17 -0400

    Update srxssh.py

--- srxssh.py
+++ srxssh.py
@@ -11,7 +11,11 @@ net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', 
 net_connect.find_prompt()
 
 #using send_command() method to send the 'show configuration' command to router
-
+print('################################\n')
+print('command show configuration output')
 output = net_connect.send_command("show configuration ")
-
+print output
+print('################################\n')
+print('command show interfaces terse output')
+output = net_connect.send_command("show interfaces terse ")
 print output
