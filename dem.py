93d3b06937a5782015305dc53726f38bf06b1dfc
kaur-raj <rajvvinder@gmail.com>
2017-07-22 07:46:37 -0400

    Update dem.py

--- dem.py
+++ dem.py
@@ -6,6 +6,15 @@ def check_bgp(net_connect, cmd='show run | inc router ospf'):
     output = net_connect.send_command_expect(cmd)
     return 'ospf' in output
 
+ for a_device in device_list:
+        net_connect = ConnectHandler(**a_device)
+        net_connect.enable()
+        print "{}: {}".format(net_connect.device_type, net_connect.find_prompt())
+        if check_ospf(net_connect):
+            print "OSPF currently configured"
+        else:
+            print "No OSPF"
+        print
 
 
 
