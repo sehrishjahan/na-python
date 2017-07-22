39b76242cce3ddf25e00964eea7ffc01ed1c1f61
kaur-raj <rajvvinder@gmail.com>
2017-07-22 08:24:56 -0400

    Update dem.py

--- dem.py
+++ dem.py
@@ -1,9 +1,9 @@
 from netmiko import ConnectHandler #imported netmiko library
 
 #initialized variables.
-platform = 'juniper'
-host = '192.168.2.15'
-username = 'juniper'
+platform = 'cisco_ios'
+host = '192.168.2.12'
+username = 'cisco1234'
 password = 'cisco1234'
 
 #calling ConnectHandler function and passing in variables
@@ -12,7 +12,7 @@ net_connect.find_prompt()
 
 #using send_command() method to send the 'show configuration' command to router
 print('\n###############################################################################\n\n')
-print('...................JUNIPER COMMAND SHOW CONFIGURATION OUTPUT....................\n\n')
+print('................................csrv2 OSPF...................\n\n')
 output = net_connect.send_config_from_file(config_file=csrv2.py)
 print output
 
