d7e2c76d4284a5a19eec8418bf0e0b836b7f04fc
kaur-raj <rajvvinder@gmail.com>
2017-07-22 07:45:18 -0400

    Update dem.py

--- dem.py
+++ dem.py
@@ -1,6 +1,10 @@
 from netmiko import ConnectHandler
 from ospfdevices import csrv2
 
+def check_bgp(net_connect, cmd='show run | inc router ospf'):
+    """Check whether BGP is currently configured on device. Return boolean"""
+    output = net_connect.send_command_expect(cmd)
+    return 'ospf' in output
 
 
 
