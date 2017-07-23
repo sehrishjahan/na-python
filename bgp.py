462f2f59841b7ae6b386d722aaa156f09bf0413a
sehrishjahan <jahan.seh@gmail.com>
2017-07-23 01:11:56 -0400

    Update bgp.py

--- bgp.py
+++ bgp.py
@@ -10,8 +10,8 @@ def main():
     device_list = [csrv, csrv2, csrv3]
     start_time = datetime.now()
     print
-    for adevice in device_list:
-        net_connect = ConnectHandler(**adevice)
+    for csrv in device_list:
+        net_connect = ConnectHandler(**csrv)
         print "{}: {}".format(net_connect.device_type, net_connect.find_prompt())
     print "Time elapsed: {}\n".format(datetime.now() - start_time)
 
