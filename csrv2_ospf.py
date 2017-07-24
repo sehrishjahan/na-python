272601a41776806b513d5d60b48c89778b24d9df
kaur-raj <rajvvinder@gmail.com>
2017-07-24 17:33:35 -0400

    Update csrv2_ospf.py

--- csrv2_ospf.py
+++ csrv2_ospf.py
@@ -69,4 +69,4 @@ def main():
 if __name__ == "__main__":
     main()
     
-print "\n\n * * * * * * * *    CONFIGURATION WAS DONE SUCCESSFULLY     * * * * * * * * *  \n"
+print "\n\n * * * * * * * * *   CONFIGURATION WAS DONE SUCCESSFULLY    * * * * * * * * * *  \n"
