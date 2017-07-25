691a31bc8a1fa5a00fea94a270ceda9b9162b7b7
kaur-raj <rajvvinder@gmail.com>
2017-07-25 00:11:34 -0400

    Update bgp.py

--- bgp.py
+++ bgp.py
@@ -38,7 +38,7 @@ def configure_bgp(net_connect, file_name=''):
         print "Error reading file: {}".format(file_name)
 
 def main():
-    device_list = [csrv]
+    device_list = [csrv2]
     print
 
     for a_device in device_list:
