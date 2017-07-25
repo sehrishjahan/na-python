7fbfed2ccff0c5d90d26cc3628792132ff8eb2b7
kaur-raj <rajvvinder@gmail.com>
2017-07-25 01:53:20 -0400

    Update ospf_csrv2.py

--- ospf_csrv2.py
+++ ospf_csrv2.py
@@ -63,7 +63,7 @@ def main():
         #file_name = 'ospf_' + device_type.split("_ssh")[0] + '.txt'
 
         # Configure OSPF
-        ospfconfig1 = configure_ospf(net_connect, 'ospfcsrv2.txt')
+        ospfconfig1 = configure_ospf(net_connect, 'ospf_csrv2.txt')
         print ospfconfig1
         output = net_connect.send_command("show ip route ")
         print output
