20871f767b7538723ba82cd541df3b3f129c66eb
kaur-raj <rajvvinder@gmail.com>
2017-07-25 18:51:06 -0400

    Update bgp_csrv1.py

--- bgp_csrv1.py
+++ bgp_csrv1.py
@@ -72,7 +72,7 @@ def main():
         else:
             print "No BGP"
         # Construct file name 
-        file_name = "bgp_" + a_device[0]['ip'] + ".txt"
+        file_name = "bgp_" + ['ip'] + ".txt"
         print file_name
         # Check BGP is now gone
         if check_bgp(net_connect):
