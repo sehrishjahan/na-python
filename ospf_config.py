4b611828ef0ed2dca0adae67b463fc9aff5bc62c
kaur-raj <rajvvinder@gmail.com>
2017-07-26 14:26:36 -0400

    Update ospf_config.py

--- ospf_config.py
+++ ospf_config.py
@@ -26,7 +26,7 @@ def check_ospf_srx_config(net_connect,cmd='show route protocol ospf'):
     output = net_connect.send_command_expect(cmd)
     return 'ospf' in output
 
-def remove_ospf_srx_config(net_connect, cmd='delete set protocol ospf area 0.0.0.0 interface ge-0/0/1.0','commit', process_id=' '):
+def remove_ospf_srx_config(net_connect, cmd='delete set protocol ospf area 0.0.0.0 interface ge-0/0/1.0', process_id=' '):
      """Remove OSPF from the config"""
     ospf_srx_cmd = "{} {}".format(cmd, str(process_id))
     cmd_srx_list = [ospf_srx_cmd]
