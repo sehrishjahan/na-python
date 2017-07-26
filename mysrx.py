d9cdc197cb76974a721bf191364d91ad0172290a
kaur-raj <rajvvinder@gmail.com>
2017-07-26 14:06:54 -0400

    Update and rename ospf_srx.py to mysrx.py

---
+++ mysrx.py
@@ -0,0 +1,15 @@
+srx1 = {    
+    'device_type': 'juniper',
+    'ip': '192.168.2.14',
+    'username': 'juniper',
+    'password': 'cisco1234',
+     
+}
+
+srx2 = {    
+    'device_type': 'juniper',
+    'ip': '192.168.2.15',
+    'username': 'juniper',
+    'password': 'cisco1234',
+     
+}
