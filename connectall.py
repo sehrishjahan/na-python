232e4841d0ce7bf0c448e0bd30f67d18bb836c8e
kaur-raj <rajvvinder@gmail.com>
2017-07-26 16:39:08 -0400

    Update connectall.py

--- connectall.py
+++ connectall.py
@@ -1,17 +1,37 @@
 from datetime import datetime
 
 from netmiko import ConnectHandler
-from mydevices import srx1, srx2
 import getpass
 
-#from devices import srx1, srx2
+print "\n\n  * * * * * * * * * * *    CONFIGURING OSPF ON SRX1    * * * * * * * * * * *  \n"
 
-def check_ospf(net_connect, cmd='show run protocol ospf'):
-    """Check whether OSPF is currently configured on device. Return boolean"""
+srx1 = {    
+    'device_type': 'juniper',
+    'ip': '192.168.2.14',
+    'username': 'juniper',
+    'password': 'cisco1234',
+     
+}
+
+srx2 = {    
+    'device_type': 'juniper',
+    'ip': '192.168.2.15',
+    'username': 'juniper',
+    'password': 'cisco1234',
+     
+}
+
+def check_ospf(net_connect, cmd='show route protocol ospf'):
+    """Check whether oospf is currently configured on device. Return boolean"""
     output = net_connect.send_command_expect(cmd)
     return 'ospf' in output
-   
-   # if net_connect.device_type == 'cisco_ios':
+
+def remove_ospf_config(net_connect, cmd='delete set protocol ospf area 0.0.0.0 interface ge-0/0/1.0', as_number=''):
+    """Remove OSPF from the config"""
+    ospf_cmd = "{} {}".format(cmd, str(as_number))
+    cmd_list = [ospf_cmd]
+    output = net_connect.send_config_set(cmd_list)
+   # if net_connect.device_type == 'cisco_xr_ssh':
     #    output += net_connect.commit()
     print output
 
@@ -27,32 +47,36 @@ def configure_ospf(net_connect, file_name=''):
 
 def main():
     device_list = [srx1, srx2]
-    print "\n              CONFIGURING OSPF PROTOCOL   "
-    print 
-    start_time = datetime.now()
     print
- # Construct file name 
-       
-        # Check OSPF is now gone
- if check_ospf(net_connect):
-         raise ValueError("OSPF configuration still detected")
-          
-        device_type = net_connect.device_type
-        file_name = "ospf_" + str(a_device ['ip']) + ".txt"
-        print "\n  Reading file : "
-        print "  {}\n".format(file_name)
-    
-    # Configure OSPF
-        bgpconfig = configure_ospf(net_connect, file_name)
-        print bgpconfig
-        print
-#         bgpconfig2 = configure_bgp(net_connect, 'bgp_csrv2.txt')
-#         print bgpconfig2
-#         bgpconfig3 = configure_bgp(net_connect, 'bgp_csrv3.txt')
-#         print bgpconfig3
-        
-    
-    print "Time elapsed: {}\n".format(datetime.now() - start_time)
 
+    for a_device in device_list:
+       # as_number = a_device.pop('as_number')
+        as_number = 100
+        net_connect = ConnectHandler(**a_device)
+        
+        net_connect.enable()
+        #print "{}: {}".format(net_connect.device_type, net_connect.find_prompt())
+        if check_ospf(net_connect):
+            print "  OSPF is currently configured !!!!!\n"
+            remove_ospf_config(net_connect, as_number=as_number)
+        else:
+            print "No OSPF"
+
+        # Check OSPF is now gone
+        if check_ospf(net_connect):
+            raise ValueError("OSPF configuration still detected")
+
+        # Construct file_name based on device_type
+        #device_type = net_connect.device_type
+        #file_name = 'ospf_' + device_type.split("_ssh")[0] + '.txt'
+
+        # Configure OSPF
+        ospfconfig1 = configure_ospf(net_connect, 'ospf_srx.txt')
+        print ospfconfig1
+        output = net_connect.send_command("show route protocol ospf ")
+        print output
+               
 if __name__ == "__main__":
     main()
+    
+print "\n\n * * * * * * * * *   CONFIGURATION WAS DONE SUCCESSFULLY    * * * * * * * * * *  \n"
