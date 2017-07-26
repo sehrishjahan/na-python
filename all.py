7c1da96e553886b32411da57b0ec1a51bdd8d049
kaur-raj <rajvvinder@gmail.com>
2017-07-25 23:53:27 -0400

    Update all.py

--- all.py
+++ all.py
@@ -1,40 +1,24 @@
-from netmiko import ConnectHandler
-from datetime import datetime
-csrv2 = {
-'device_type':'cisco_ios',
-'ip': '192.168.2.12',
-'username': 'cisco',
-'password':'cisco1234',
-'verbose':False,
+
+csrv1 = {    
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.11',
+    'username': 'csrv',
+    'password': 'telnet',
+    'secret': 'cisco',
 }
-csrv3 = {
-'device_type':'cisco_ios',
-'ip': '192.168.2.13',
-'username': 'cisco',
-'password':'cisco1234',
-'verbose':False,
+
+csrv2 = {    
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.12',
+    'username': 'cisco',
+    'password': 'cisco1234',
+    'secret': 'cisco1234',
 }
-juniper_srx = {
-'device_type':'juniper',
-'ip': '192.168.2.14',
-'username': 'juniper',
-'password':'cisco1234',
-'verbose':False,
+
+csrv3 = {   
+    'device_type': 'cisco_ios',
+    'ip': '192.168.2.13',
+    'username': 'cisco',
+    'password': 'cisco1234',
+    'secret': 'cisco1234',
 }
-juniper_srx2 = {
-'device_type':'juniper',
-'ip': '192.168.2.15',
-'username': 'juniper',
-'password':'cisco1234',
-'verbose':False,
-}
-all_devices =[csrv2, csrv3, juniper_srx, juniper_srx2]
-start_time = datetime.now()
-for a_device in all_devices:
-     net_connect = ConnectHandler(**a_device)
-     output = net_connect.send_command("show arp")
-     print"\n\n^^^^^^ Device{0}^^^^^^^".format(a_device['device_type'])
-     print output
-     print">>>>>>>END>>>>>>>>>"
-end_time = datetime.now()
-total_time = end_time - start_time
