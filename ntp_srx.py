e1475eee2efd90a3fc0d13930512f4c2ecfbe6ff
kaur-raj <rajvvinder@gmail.com>
2017-07-30 18:48:11 -0400

    Update ntp_srx.py

--- ntp_srx.py
+++ ntp_srx.py
@@ -1,8 +1,8 @@
-from datetime import datetime
-
 from netmiko import ConnectHandler
-from mydevices import srx1, srx2
+from mydevices import srx1,srx2
 import getpass
+
+#from devices import csrv, csrv2, csrv3
 
 def configure_ntp(net_connect, file_name=''):
     """Configure NTP on device."""
@@ -14,24 +14,31 @@ def configure_ntp(net_connect, file_name=''):
 
 def main():
     device_list = [srx1, srx2]
-    print
-
+    print "\n              CONFIGURING NTP PROTOCOL   "
+    print 
+#     file_list = ['bgp_csrv1.txt', 'bgp_csrv2.txt', 'bgp_csrv3.txt'] 
+  
     for a_device in device_list:
-       
         net_connect = ConnectHandler(**a_device)
         
         net_connect.enable()
-        #print "{}: {}".format(net_connect.device_type, net_connect.find_prompt())
+        #print "{}: {}".format(net_connect.device_type, net_connect.find_prompt()
        
-        if check_ntp(net_connec
-        # Construct file_name based on device_type
-        #device_type = net_connect.device_type
-        #file_name = 'ospf_' + device_type.split("_ssh")[0] + '.txt'
-
-        # Configure OSPF
-        ospfconfig1 = configure_ospf(net_connect, 'ospf_srx1.txt')
-        print ospfconfig1             
-if __name__ == "__main__":
-    main()
+         
+        device_type = net_connect.device_type
+        file_name = "ntp_" + (a_device ['username']) + ".txt"
+        print "\n  Reading file : "
+        print "  {}\n".format(file_name)
     
-print "\n\n * * * * * * * * *   CONFIGURATION WAS DONE SUCCESSFULLY    * * * * * * * * * *  \n"
+    # Configure NTP
+        ntpconfig = configure_ntp(net_connect, file_name)
+        print ntpconfig
+        print
+        print"\n    CLOCK TIMING"
+        output = net_connect.send_command("show clock")
+        print output
+
+if __name__ == "__main__":
+  main()
+
+print "\n\n  * * * * * * * * * * *    NTP SUCCESSFULLY CONFIGURED    * * * * * * * * * * *  \n"
