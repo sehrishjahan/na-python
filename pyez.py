e06e78278802b44e012707c095fd8f110efae243
Harman <harmanpreetsingh.gil@ryerson.ca>
2017-08-05 15:41:54 -0400

    modified pyez file

--- pyez.py
+++ pyez.py
@@ -1,8 +1,25 @@
-import jnpr.junos
-from jnpr.junos.device import Device
-junos_host = '192.168.2.14'
-dev = Device(host=str(junos_host), user='juniper', password='cisco1234')
-dev.open()
-print dev.facts
-dev.close()
+from jnpr.junos import Device
+from getpass import getpass
+import sys
 
+hostname = '192.168.2.14'
+username = 'juniper'
+password = 'cisco1234'
+
+# NETCONF session over SSH
+dev = Device(host=hostname, user=username, passwd=password)
+
+# Telnet connection
+#dev = Device(host=hostname, user=username, passwd=password, mode='telnet', port='23', gather_facts=True)
+
+# Serial console connection
+#dev = Device(host=hostname, user=username, passwd=password, mode='serial', port='/dev/ttyUSB0', gather_facts=True)
+
+try:
+    dev.open()
+except Exception as err:
+    print (err)
+    sys.exit(1)
+
+print (dev.facts)
+dev.close()
