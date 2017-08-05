266a15487201af30c39011eba00e8cdbd3f03078
Harman <harmanpreetsingh.gil@ryerson.ca>
2017-08-05 15:20:53 -0400

    modified pyez file

--- pyez.py
+++ pyez.py
@@ -1,7 +1,7 @@
 import jnpr.junos
 from jnpr.junos.device import Device
 junos_host = '192.168.2.14'
-dev = Device(host=junos_host, user='juniper', password='cisco1234')
+dev = Device(host=str(junos_host), user='juniper', password='cisco1234')
 dev.open()
 print dev.facts
 dev.close()
