ddac04f2f488985efdf8152b8f9ab59f6face121
sehrishjahan <jahan.seh@gmail.com>
2017-08-05 14:44:39 -0400

    Update pyez.py

--- pyez.py
+++ pyez.py
@@ -1,2 +1,9 @@
 from jnpr.junos.device import Device
-if__name__=='__main'__
+if __name__ == '__main__':
+     junos_hosts = ['192.168.2.14']
+     for ip in junos_hosts:
+             dev = Device(host=ip, user='juniper', password='cisco1234')
+             dev.open()
+             print dev.facts
+             dev.close()
+
