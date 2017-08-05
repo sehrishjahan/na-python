4214bcc00b3505e20d76fd67d0afa0637a8f8e27
sehrishjahan <jahan.seh@gmail.com>
2017-08-05 15:07:17 -0400

    Update pyez.py

--- pyez.py
+++ pyez.py
@@ -1,5 +1,5 @@
 import jnpr.junos
-from jnpr.junos.device import
+from jnpr.junos.device import Device
 junos_host = '192.168.2.14'
 dev = Device(host=junos_host, user='juniper', password='cisco1234')
 dev.open()
