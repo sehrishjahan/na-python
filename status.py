29f4b62d36dcdcab09420f181b0464befd120612
kaur-raj <rajvvinder@gmail.com>
2017-08-06 15:56:37 -0400

    Update status.py

--- status.py
+++ status.py
@@ -15,6 +15,34 @@ net_connect.find_prompt()
 output = net_connect.send_command("show ip int brief")
 print output
 
+platform = 'cisco_ios'
+host = '192.168.2.12'
+username = 'cisco'
+ 
+
+ #calling ConnectHandler function and passing in variables
+
+net_connect = ConnectHandler(device_type=platform, ip=host, username='cisco', password='cisco1234')
+net_connect.find_prompt()
+ 
+ #using send_command() method to send the 'show ip int brief' command to router
+output = net_connect.send_command("show ip int brief")
+print output
+
+platform = 'cisco_ios'
+host = '192.168.2.13'
+username = 'cisco'
+ 
+
+ #calling ConnectHandler function and passing in variables
+
+net_connect = ConnectHandler(device_type=platform, ip=host, username='cisco', password='cisco1234')
+net_connect.find_prompt()
+ 
+ #using send_command() method to send the 'show ip int brief' command to router
+output = net_connect.send_command("show ip int brief")
+print output
+
 platform = 'juniper'
 host = '192.168.2.15'
 username = 'juniper'
