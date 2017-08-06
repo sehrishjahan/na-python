2016deede86e1ad9c09b0ef3e3bfdd6c67a2accb
kaur-raj <rajvvinder@gmail.com>
2017-08-06 14:48:26 -0400

    Update t.py

--- t.py
+++ t.py
@@ -12,3 +12,6 @@ remote_conn_pre.set_missing_host_key_policy(
    paramiko.AutoAddPolicy())
  
 remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
+remote_conn = remote_conn_pre.invoke_shell()
+output = remote_conn.recv(1000)
+print output
