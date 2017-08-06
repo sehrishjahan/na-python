ab2f96d73f5114c9e4cdd9fffea9869864f8c16d
kaur-raj <rajvvinder@gmail.com>
2017-08-06 15:45:34 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -14,11 +14,6 @@ net_connect.find_prompt()
 output = net_connect.send_command("show ip int brief")
 print output
 
-platform = 'juniper'
-host = '192.168.2.15'
-username = 'juniper'
-password = 'cisco1234'
-
 #calling ConnectHandler function and passing in variables
 net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', password='cisco1234')
 net_connect.find_prompt()
