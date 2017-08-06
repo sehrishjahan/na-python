6d420b262504794201951a19a5f30dda697e0efa
kaur-raj <rajvvinder@gmail.com>
2017-08-06 01:24:52 -0400

    Update v.py

--- v.py
+++ v.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from netmiko import ConnectHandler
-from mydevices import csrv2, csrv3
+from mydevices import csrv1, csrv2, csrv3
 import getpass
 def check_ospf(net_connect, cmd='show run | inc router bgp'):
     """Check whether OSPF is currently configured on device. Return boolean"""
@@ -23,7 +23,7 @@ def configure_ospf(net_connect, file_name=''):
         print "Error reading file: {}".format(file_name)
 
 def main():
-    device_list = [csrv2, csrv3,]
+    device_list = [csrv1, csrv2, csrv3,]
     print "\n              CONFIGURING OSPF PROTOCOL   "
     print 
     start_time = datetime.now()
