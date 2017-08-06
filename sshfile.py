70f081e69b27a0603c477206e754620bafeb655c
kaur-raj <rajvvinder@gmail.com>
2017-08-06 15:51:43 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -5,10 +5,7 @@ platform = 'cisco_ios'
 host = '192.168.2.11'
 username = 'cisco'
  
-platform = 'juniper'
-host = '192.168.2.15'
-username = 'juniper'
-password = 'cisco1234'
+
  #calling ConnectHandler function and passing in variables
 
 net_connect = ConnectHandler(device_type=platform, ip=host, username='cisco', password='cisco1234')
