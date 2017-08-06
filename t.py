79d7ea05c434ed69750690a833dd2a11fd30f560
sehrishjahan <jahan.seh@gmail.com>
2017-08-06 15:02:02 -0400

    Update t.py

--- t.py
+++ t.py
@@ -1,20 +1,17 @@
 import paramiko
- 
+
 ip = '192.168.2.12'
 username = 'cisco'
 password = 'cisco1234'
  
-remote_conn_pre=paramiko.SSHClient()
-remote_conn_pre
+ssh=paramiko.SSHClient()
+
+
 
  
-remote_conn_pre.set_missing_host_key_policy(
-   paramiko.AutoAddPolicy())
- 
-remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-remote_conn = remote_conn_pre.invoke_shell()
-output = remote_conn.recv(1000)
-print output
-remote_conn.send("show ip int brief\n")
-output = remote_conn.recv(5000)
-print output
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(ip, username=username, password=password)
+stdin, stdout, stderr = ssh.exec_command('show ip int br')
+
+output = stdout.readlines()
+print \n.joinoutput()
