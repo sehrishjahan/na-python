055b39afe825e61165ce1b5dc605691b6bcb47ae
kaur-raj <rajvvinder@gmail.com>
2017-08-06 16:02:07 -0400

    Update status.py

--- status.py
+++ status.py
@@ -44,6 +44,25 @@ output = net_connect.send_command("show ip int brief")
 print output
 
 platform = 'juniper'
+host = '192.168.2.14'
+username = 'juniper'
+password = 'cisco1234'
+
+#calling ConnectHandler function and passing in variables
+net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', password='cisco1234')
+net_connect.find_prompt()
+
+#using send_command() method to send the 'show configuration' command to router
+print('...................JUNIPER COMMAND SHOW CONFIGURATION OUTPUT....................\n\n')
+print('--------------------------------------------------------------------------------\n')
+output = net_connect.send_command("show configuration ")
+print output
+print('...................JUNIPER COMMAND SHOW INTERFACES TERSE OUTPUT................\n\n')
+print('--------------------------------------------------------------------------------\n')
+output = net_connect.send_command("show interfaces terse ")
+print output
+
+platform = 'juniper'
 host = '192.168.2.15'
 username = 'juniper'
 password = 'cisco1234'
@@ -53,11 +72,11 @@ net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', 
 net_connect.find_prompt()
 
 #using send_command() method to send the 'show configuration' command to router
-print('\n###############################################################################\n\n')
 print('...................JUNIPER COMMAND SHOW CONFIGURATION OUTPUT....................\n\n')
+print('--------------------------------------------------------------------------------\n')
 output = net_connect.send_command("show configuration ")
 print output
-print('\n################################################################################\n\n')
 print('...................JUNIPER COMMAND SHOW INTERFACES TERSE OUTPUT................\n\n')
+print('--------------------------------------------------------------------------------\n')
 output = net_connect.send_command("show interfaces terse ")
 print output
