abb87f47983c55fce3d62e3c2747718834727764
kaur-raj <rajvvinder@gmail.com>
2017-08-06 15:48:55 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -17,7 +17,7 @@ print output
 platform = 'juniper'
 host = '192.168.2.15'
 username = 'juniper'
-password = 'cisco1234
+password = 'cisco1234'
 
 #calling ConnectHandler function and passing in variables
 net_connect = ConnectHandler(device_type=platform, ip=host, username='juniper', password='cisco1234')
