f0d18132aa3c3fc98a28f3e53ab3d8949d65cf9e
kaur-raj <rajvvinder@gmail.com>
2017-08-06 15:34:20 -0400

    Update sshfile.py

--- sshfile.py
+++ sshfile.py
@@ -3,10 +3,10 @@ from netmiko import ConnectHandler #imported netmiko library
 #initialized variables.
 platform = 'cisco_ios'
 host = '192.168.2.11'
-username = 'csrv'
+username = 'cisco'
 
 #calling ConnectHandler function and passing in variables
-net_connect = ConnectHandler(device_type=platform, ip=host, username='csrv', password='telnet')
+net_connect = ConnectHandler(device_type=platform, ip=host, username='cisco', password='cisco1234')
 net_connect.find_prompt()
 
 #using send_command() method to send the 'show ip int brief' command to router
